efbb2b8fc5348bfd2eca13aeed204e819cda55d2
Dhimas Bayu Ilham Ramadhan <dhimasbayu.9f@gmail.com>
2020-10-15 15:25:57 +0700

    init login page and add some file image

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/index.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row justify-content-md-center mt-5">
+        <div class="col-lg-6 image-view">
+          <div class="text-center" id="image-content">
+            <img src="./img/product_manager_blue.svg" class="rounded" alt="">
+            <h3>Free up your mental space</h3>
+            <p>Regain clarity and calmness by getting all those tasks out of your head and onto your to-do list (no matter where you are or what device you use).</p>
+          </div>
+        </div>
+        <div class="col-lg-5" id="form-view">
+          <form action="index.html" method="post">
+            <div class="form-group mt-5">
+              <label for="inputUsername">Username</label>
+              <input type="text" class="form-control" name="inputUsername" id="inputUsername" value="">
+            </div>
+            <div class="form-group">
+              <label for="inputPassword">Password</label>
+              <input type="password" class="form-control" name="inputPassword" id="inputPassword" value="">
+            </div>
+            <button type="submit" class="btn" name="button">Sign In</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+  <footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </footer>
+</html>
